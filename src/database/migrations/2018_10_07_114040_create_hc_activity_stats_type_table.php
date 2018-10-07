b50cb8314879f670abb576c4349435dbe0eaafaa
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-10-07 17:12:11 +0300

    Added migrations

---
+++ src/database/migrations/2018_10_07_114040_create_hc_activity_stats_type_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHcActivityStatsTypeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hc_activity_stats_type', function(Blueprint $table) {
+            $table->increments('count');
+            $table->uuid('id')->unique();
+            $table->datetime('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->datetime('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->datetime('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hc_activity_stats_type');
+    }
+}
---
+++ src/database/migrations/2018_10_07_114041_create_hc_activity_stats_detailed_data_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHcActivityStatsDetailedDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hc_activity_stats_detailed_data', function(Blueprint $table) {
+            $table->increments('count');
+            $table->uuid('id')->unique();
+            $table->datetime('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->datetime('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->datetime('deleted_at')->nullable();
+
+            $table->uuid('type_id');
+
+            $table->string('amountable_id');
+            $table->uuid('amountable_type');
+
+            $table->dateTime('date');
+            $table->integer('amount');
+
+            $table->foreign('type_id')->references('id')->on('hc_activity_stats_type')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('hc_activity_stats_detailed_data', function(Blueprint $table) {
+            $table->dropForeign(['type_id']);
+        });
+
+        Schema::dropIfExists('hc_activity_stats_detailed_data');
+    }
+}
