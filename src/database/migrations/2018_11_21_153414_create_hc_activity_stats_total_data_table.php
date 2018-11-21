487d8e663fd2edae0bc90b80442e3c16f7ff3c79
joanabr <38035924+joanabr@users.noreply.github.com>
2018-11-21 15:40:51 +0200

    added total field

---
+++ src/database/migrations/2018_11_21_153414_create_hc_activity_stats_total_data_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHcActivityStatsTotalDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hc_activity_stats_total_data', function(Blueprint $table) {
+            $table->increments('count');
+            $table->uuid('id')->unique();
+            $table->datetime('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->datetime('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->datetime('deleted_at')->nullable();
+
+            $table->uuid('type_id');
+
+            $table->string('amountable_id');
+            $table->uuid('amountable_type');
+
+            $table->year('date');
+            $table->integer('amount');
+
+            $table->foreign('type_id')->references('id')->on('hc_activity_stats_type');
+            $table->unique(['type_id', 'date', 'amountable_id', 'amountable_type'], 'total_activity_stats_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('hc_activity_stats_years_data', function(Blueprint $table) {
+            $table->dropForeign(['type_id']);
+        });
+
+        Schema::dropIfExists('hc_activity_stats_years_data');
+    }
+}
